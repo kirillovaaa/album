e88b9b798fe6cfd5104a9ed5fb7f80849c8ee93c
Aleksandra Kirillova <kiri2lova@gmail.com>
2023-09-07 14:24:03 +0800

    fix: правки

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,18 +1,22 @@
 import React from "react";
-import indonesia from "../../images/indonesia-promo.png";
+// import indonesia from "../../images/indonesia-promo.png";
+import sea from "../../images/sea.jpeg";
 import "./Promo.css";
 
 function Promo() {
   return (
     <section className="promo">
       <div className="promo__wrapper">
-        <img src={indonesia} className="promo__image" alt="indonesia" />
+        <img src={sea} className="promo__image" alt="indonesia" />
         <div className="promo__information">
+          <h3 className="promo__place">
+            <span class="colortext">KAS TURKEY</span>
+          </h3>
           <h2 className="promo__title">
             I can show you
             <br /> more!
           </h2>
-          <p className="promo__tdescription">
+          <p className="promo__description">
             Hi, I'm Sasha and I travel a lot and I'll show you the album that I
             created in these 2 years
           </p>
